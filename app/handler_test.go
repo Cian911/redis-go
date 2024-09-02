47baf33cdbaaef38ff69e7681f3e70afb89bdc98
Cian Gallagher <cian.gallagher@zendesk.com>
2024-09-02 23:51:02 +0100

    add handler
    Signed-off-by: Cian Gallagher <cian.gallagher@zendesk.com>

--- app/handler_test.go
+++ app/handler_test.go
@@ -1,9 +1,75 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func TestHandler(t *testing.T) {
-  t.Run("Handles the correct command", func(t *testing.T) {
+  t.Run("echo", func(t *testing.T) {
+    want := token{
+      typ: string(STRING), 
+      val: "hey",
+    }
 
+    tok := token{
+			typ: string(ARRAY),
+			array: []token{
+				{
+					typ:  string(BULK),
+					bulk: "ECHO",
+				},
+				{
+					typ:  string(BULK),
+					bulk: "hey",
+				},
+			},
+		}
+
+    command := strings.ToUpper(tok.array[0].bulk)
+    args := tok.array[1:]
+
+    handler, ok := Handlers[command]
+    if !ok {
+      t.Errorf("Could not get handler. wanted %s, got %s", "echo", command)
+    }
+
+    result := handler(args)
+
+    if !reflect.DeepEqual(result, want) {
+      t.Errorf("Failed echo. wanted %v, got %v", result, want)
+    }
+  })
+
+  t.Run("ping", func(t *testing.T) {
+    want := token{
+      typ: string(STRING), 
+      val: "PONG",
+    }
+
+    tok := token{
+			typ: string(ARRAY),
+			array: []token{
+				{
+					typ:  string(BULK),
+					bulk: "ping",
+				},
+			},
+		}
+
+    command := strings.ToUpper(tok.array[0].bulk)
+    args := tok.array[1:]
+
+    handler, ok := Handlers[command]
+    if !ok {
+      t.Errorf("Could not get handler. wanted %s, got %s", "echo", command)
+    }
+
+    result := handler(args)
+
+    if !reflect.DeepEqual(result, want) {
+      t.Errorf("Failed ping. wanted %v, got %v", result, want)
+    }
   })
 }
