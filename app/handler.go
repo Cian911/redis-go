e8fa0cb80e018c5fecde25103207759e6983c712
Cian Gallagher <cian.gallagher@zendesk.com>
2024-09-05 00:03:55 +0100

    begin support for expiry
    Signed-off-by: Cian Gallagher <cian.gallagher@zendesk.com>

--- app/handler.go
+++ app/handler.go
@@ -1,6 +1,12 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
 
 var Handlers = map[string]func([]token) token{
 	"PING": ping,
@@ -10,9 +16,18 @@ var Handlers = map[string]func([]token) token{
 }
 
 var (
-	datastore = map[string]string{}
-	mux       = &sync.RWMutex{}
+	datastore = map[string]object{}
+	// Mutex is short for mutal-exclusion
+	// A mutex keeps track of which thread has access to which
+	// variable at any given time
+	mux = &sync.RWMutex{}
 )
+
+type object struct {
+	value     string
+	createdAt time.Time
+	expiry    int // In Milliseconds
+}
 
 func echo(args []token) token {
 	if len(args) == 0 {
@@ -34,10 +49,39 @@ func set(args []token) token {
 	if len(args) < 2 {
 		return token{typ: string(ERROR), val: "Set needs two values"}
 	}
-	// Create lock to avoid race-conditions
-	mux.Lock()
-	datastore[args[0].bulk] = args[1].bulk
-	mux.Unlock()
+
+	// Check if we need to set expiry
+	if len(args) >= 4 && strings.ToUpper(args[2].bulk) == "PX" {
+		fmt.Printf("%v\n", args[3].num)
+		exp, err := strconv.Atoi(args[3].val)
+		if err != nil {
+			return token{typ: string(ERROR), val: "Could not convert expiry time"}
+		}
+		panic("here")
+
+		// Proceed with setting the expiry
+		mux.Lock()
+		datastore[args[0].bulk] = object{
+			value:     args[1].bulk,
+			createdAt: time.Now().UTC(),
+		}
+		mux.Unlock()
+
+		time.AfterFunc(time.Duration(exp)*time.Millisecond, func() {
+			// Delete the key after it's expired
+			mux.Lock()
+			delete(datastore, args[0].bulk)
+			mux.Unlock()
+		})
+	} else {
+		// Create lock to avoid race-conditions
+		mux.Lock()
+		datastore[args[0].bulk] = object{
+			value:     args[1].bulk,
+			createdAt: time.Now().UTC(),
+		}
+		mux.Unlock()
+	}
 
 	return token{typ: string(STRING), val: "OK"}
 }
@@ -48,8 +92,12 @@ func get(args []token) token {
 	}
 
 	mux.RLock()
-	val := datastore[args[0].bulk]
+	obj := datastore[args[0].bulk]
 	mux.RUnlock()
 
-	return token{typ: string(STRING), val: val}
+	if obj.value == "" {
+		return token{typ: string(ERROR)}
+	}
+
+	return token{typ: string(STRING), val: obj.value}
 }
--- app/handler_test.go
+++ app/handler_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 func TestHandler(t *testing.T) {
-  t.Run("echo", func(t *testing.T) {
-    want := token{
-      typ: string(STRING), 
-      val: "hey",
-    }
+	t.Run("echo", func(t *testing.T) {
+		want := token{
+			typ: string(STRING),
+			val: "hey",
+		}
 
-    tok := token{
+		tok := token{
 			typ: string(ARRAY),
 			array: []token{
 				{
@@ -27,28 +27,28 @@ func TestHandler(t *testing.T) {
 			},
 		}
 
-    command := strings.ToUpper(tok.array[0].bulk)
-    args := tok.array[1:]
+		command := strings.ToUpper(tok.array[0].bulk)
+		args := tok.array[1:]
 
-    handler, ok := Handlers[command]
-    if !ok {
-      t.Errorf("Could not get handler. wanted %s, got %s", "echo", command)
-    }
+		handler, ok := Handlers[command]
+		if !ok {
+			t.Errorf("Could not get handler. wanted %s, got %s", "echo", command)
+		}
 
-    result := handler(args)
+		result := handler(args)
 
-    if !reflect.DeepEqual(result, want) {
-      t.Errorf("Failed echo. wanted %v, got %v", result, want)
-    }
-  })
+		if !reflect.DeepEqual(result, want) {
+			t.Errorf("Failed echo. wanted %v, got %v", result, want)
+		}
+	})
 
-  t.Run("ping", func(t *testing.T) {
-    want := token{
-      typ: string(STRING), 
-      val: "PONG",
-    }
+	t.Run("ping", func(t *testing.T) {
+		want := token{
+			typ: string(STRING),
+			val: "PONG",
+		}
 
-    tok := token{
+		tok := token{
 			typ: string(ARRAY),
 			array: []token{
 				{
@@ -58,18 +58,92 @@ func TestHandler(t *testing.T) {
 			},
 		}
 
-    command := strings.ToUpper(tok.array[0].bulk)
-    args := tok.array[1:]
+		command := strings.ToUpper(tok.array[0].bulk)
+		args := tok.array[1:]
 
-    handler, ok := Handlers[command]
-    if !ok {
-      t.Errorf("Could not get handler. wanted %s, got %s", "echo", command)
-    }
+		handler, ok := Handlers[command]
+		if !ok {
+			t.Errorf("Could not get handler. wanted %s, got %s", "echo", command)
+		}
 
-    result := handler(args)
+		result := handler(args)
 
-    if !reflect.DeepEqual(result, want) {
-      t.Errorf("Failed ping. wanted %v, got %v", result, want)
-    }
-  })
+		if !reflect.DeepEqual(result, want) {
+			t.Errorf("Failed ping. wanted %v, got %v", result, want)
+		}
+	})
+
+	t.Run("set", func(t *testing.T) {
+		want := token{
+			typ: string(STRING),
+			val: "OK",
+		}
+
+		tok := token{
+			typ: string(SET),
+			array: []token{
+				{
+					typ:  string(BULK),
+					bulk: "SET",
+				},
+				{
+					typ:  string(BULK),
+					bulk: "key",
+				},
+				{
+					typ:  string(BULK),
+					bulk: "value",
+				},
+			},
+		}
+
+		command := strings.ToUpper(tok.array[0].bulk)
+		args := tok.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			t.Errorf("Could not get handler. wanted %s, got %s", "echo", command)
+		}
+
+		result := handler(args)
+
+		if !reflect.DeepEqual(result, want) {
+			t.Errorf("Failed set. wanted %v, got %v", result, want)
+		}
+	})
+
+	t.Run("get", func(t *testing.T) {
+		want := token{
+			typ: string(STRING),
+			val: "value",
+		}
+
+		tok := token{
+			array: []token{
+				{
+					typ:  string(BULK),
+					bulk: "GET",
+				},
+				{
+					typ:  string(BULK),
+					bulk: "key",
+				},
+			},
+		}
+
+		command := strings.ToUpper(tok.array[0].bulk)
+		args := tok.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			t.Errorf("Could not get handler. wanted %s, got %s", "echo", command)
+		}
+
+		result := handler(args)
+
+		// Should be true as set was called in previous run step
+		if !reflect.DeepEqual(result, want) {
+			t.Errorf("Failed get. wanted %v, got %v", result, want)
+		}
+	})
 }
